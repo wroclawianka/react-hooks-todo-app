dde601c4e1119756df845d5fe2566e964c419ae0
Dorota Zelga <dorota.zelga@gmail.com>
2020-04-04 14:52:34 +0200

    comment

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,16 +1,27 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
 
-const ResourceList = ({resource}) => {
-    const [resources, setResources] = useState([]);
+const fetchResource = async (resource) => {
+    return await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
+};
 
+const useResources = (resource) => {
+
+};
+
+const ResourceList = ({resource}) => {
+    // VERSION 2.
+    const resources = useResources(resource);
+
+   /* VERSION 1.
+    const [resources, setResources] = useState([]);
     const fetchResource = async (resource) => {
         return await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
     };
-
     useEffect(() => {
         fetchResource(resource).then(res => setResources(res.data));
-    }, [resource]);
+    }, [resource]);*/
+
     /* why needed? thx to that useEffect will not fetch
      resource if value of resource did not change
      one tricky case is object, {color: 'red'} and {color: 'red'} are not the same for JS */
